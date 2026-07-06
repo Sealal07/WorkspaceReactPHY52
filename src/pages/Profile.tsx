0508ca888ec65d057354bb6c7daccccb3f99316a
Александра Рудак <rudak_a@ItTopNSK.local>
2026-07-06 21:25:42 +0700

    feat: develop dashboard module

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import React, {use, useContext, useState} from "react";
+import React, {useContext, useState} from "react";
 import { UserContext } from "../context/UserContext";
 
 interface BookingRecord {
