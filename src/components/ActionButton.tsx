6f9d8161ee7f8efb6283eb2f2ca5255b40367322
Александра Рудак <rudak_a@ItTopNSK.local>
2026-07-06 18:39:59 +0700

    feat: sprint 2  booking catalog

---
+++ src/components/ActionButton.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface ActionButtonProps{
+    onAction: () => void;
+    label: string;
+}
+
+export const ActionButton: React.FC<ActionButtonProps> = ({onAction, label})=>{
+    return (
+        <button
+            onClick={(e)=>{
+                e.stopPropagation();
+                onAction();
+            }}
+        >{label}</button>
+    );
+};
